84bef9c21410334e09ae5fc3c8ff47ebcb9f5d64
nessxellos <ness_xellos@naver.com>
2021-08-31 14:26:49 +0900

    detail design

--- app/src/main/java/com/cos/playground/View/Community/CBoardDetailActivity.java
+++ app/src/main/java/com/cos/playground/View/Community/CBoardDetailActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.cos.playground.Controller.BoardController;
@@ -15,6 +17,9 @@ import com.cos.playground.R;
 import com.cos.playground.config.SessionUser;
 import com.google.android.material.button.MaterialButton;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,8 +29,10 @@ public class CBoardDetailActivity extends AppCompatActivity {
     private static final String TAG = "cBoard";
     private CBoardDetailActivity mContext = CBoardDetailActivity.this;
 
-    private MaterialButton btnDelete, btnUpdateForm;
-    private TextView tvBox;
+    private Button btnDelete, btnUpdateForm;
+    private TextView tvDetailUsername, tvDetailEmail, tvDetailCategory, tvDetailRegdate,
+                    tvDetailTitle, tvDetailContent, tvViewCount, tvLikeCount, ic_Like, ic_Reply;
+    private ImageView ivUserProfile, ivFile;
 
     private BoardController boardController;
     private CBoard cBoard;
@@ -50,7 +57,16 @@ public class CBoardDetailActivity extends AppCompatActivity {
         boardController = new BoardController();
         btnDelete = findViewById(R.id.btnDelete);
         btnUpdateForm = findViewById(R.id.btnUpdateForm);
-        tvBox = findViewById(R.id.tvBox);
+        tvDetailUsername = findViewById(R.id.tvDetailUsername);
+        tvDetailEmail = findViewById(R.id.tvDetailEmail);
+        tvDetailCategory = findViewById(R.id.tvDetailCategory);
+        tvDetailRegdate = findViewById(R.id.tvDetailRegdate);
+        tvDetailTitle = findViewById(R.id.tvDetailTitle);
+        tvDetailContent = findViewById(R.id.tvDetailContent);
+        tvViewCount = findViewById(R.id.tvViewCount);
+        tvLikeCount = findViewById(R.id.tvLikeCount);
+        ic_Like = findViewById(R.id.ic_Like);
+        ic_Reply = findViewById(R.id.ic_Reply);
     }
 
     public void initLr(){
@@ -107,17 +123,18 @@ public class CBoardDetailActivity extends AppCompatActivity {
                     btnDelete.setVisibility(View.GONE);
                     btnUpdateForm.setVisibility(View.GONE);
                 }
+                Date from = cm.getData().getRegdate();
+                SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                String date = transFormat.format(from);
                 cBoard = cm.getData();
-                tvBox.setText("");
-                tvBox.append("id: "+cm.getData().getId()+"\n");
-                tvBox.append("title: "+cm.getData().getTitle()+"\n");
-                tvBox.append("content: "+cm.getData().getContent()+"\n");
-                tvBox.append("regdate: "+cm.getData().getRegdate()+"\n");
-                tvBox.append("file: "+cm.getData().getFile()+"\n");
-                tvBox.append("favCount: "+cm.getData().getFavCount()+"\n");
-                tvBox.append("viewCount: "+cm.getData().getViewCount()+"\n");
-                tvBox.append("userId: "+cm.getData().getUserId()+"\n");
-                tvBox.append("writer: "+cm.getData().getWriter()+"\n");
+                tvDetailTitle.setText(cm.getData().getTitle());
+                tvDetailEmail.setText("");
+                tvDetailCategory.setText(cm.getData().getCategory());
+                tvDetailRegdate.setText(date);
+                tvDetailUsername.setText(cm.getData().getWriter());
+                tvDetailContent.setText(cm.getData().getContent());
+                tvViewCount.setText("조회수 :"+cm.getData().getViewCount());
+                tvLikeCount.setText("좋아요 : "+cm.getData().getFavCount());
             }
 
             @Override
