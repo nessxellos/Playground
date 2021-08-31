10535051c257b383142e5621a1586858e9f0c45a
nessxellos <ness_xellos@naver.com>
2021-08-31 20:34:31 +0900

    main, write

--- app/src/main/java/com/cos/playground/View/Community/CBoardListActivity.java
+++ app/src/main/java/com/cos/playground/View/Community/CBoardListActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cos.playground.Controller.BoardController;
 import com.cos.playground.Controller.DTO.CMRespDto;
 import com.cos.playground.Model.CBoard;
 import com.cos.playground.R;
 import com.cos.playground.View.Community.adapter.CBoardListAdapter;
+import com.cos.playground.View.auth.LoginActivity;
+import com.cos.playground.config.SessionUser;
 
 import java.util.List;
 
@@ -26,8 +31,22 @@ public class CBoardListActivity extends AppCompatActivity {
     private RecyclerView rvCBoardList;
     private RecyclerView.LayoutManager rvLayoutManager;
 
+    private Button btnLogin, btnWrite;
+
     private CBoardListAdapter cBoardListAdapter;
     private BoardController boardController;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (SessionUser.sessionId == null) {
+            btnWrite.setVisibility(View.GONE);
+            btnLogin.setVisibility(View.VISIBLE);
+        } else {
+            btnLogin.setVisibility(View.GONE);
+            btnWrite.setVisibility(View.VISIBLE);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +62,27 @@ public class CBoardListActivity extends AppCompatActivity {
     public void init() {
         boardController = new BoardController();
         rvCBoardList = findViewById(R.id.rvCBoardList);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnWrite = findViewById(R.id.btnWrite);
     }
 
     public void initLr() {
-
+        btnWrite.setOnClickListener(v-> {
+                    Intent intent = new Intent(
+                            mContext,
+                            CBoardWriteActivity.class
+                    );
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                });
+        btnLogin.setOnClickListener(v->{
+            Intent intent = new Intent(
+                    mContext,
+                    LoginActivity.class
+            );
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        });
     }
 
     public void initAdapter(){
